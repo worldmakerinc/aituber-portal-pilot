640670b766150cec4dfd76c5fcb73d0cc8604015
Harxxki <haruki4dev@gmail.com>
2023-08-17 19:57:29 +0900

    rm logout page

--- components/LogoutButton.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { signOut } from 'next-auth/react'
-import { FC } from 'react'
-
-export const LogoutButton: FC = () => (
-  <button onClick={() => signOut()}>ログアウト</button>
-)
